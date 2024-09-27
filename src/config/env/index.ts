d612582bb65e559be2d8915ff43a54ebc1342785
Maria Eduarda Ferreira <MEFerreira@brasal.com.br>
2024-09-27 11:10:53 -0300

    ajuste teste ping

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
-  NODE_ENV: z.enum(['development', 'production', 'test ']).default('production')
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('production')
 });
 
 const _env = envSchema.safeParse(process.env);
