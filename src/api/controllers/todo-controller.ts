0f2c7643b22bb02ed7884c33e4256bc475b1a881
Meferreira <mariaeduardaferreiraa7h@gmail.com>
2024-10-02 16:09:49 -0300

    feat: implements set todo has done route

--- src/api/controllers/todo-controller.ts
+++ src/api/controllers/todo-controller.ts
@@ -60,7 +60,15 @@ export class TodoController {
     request: FastifyRequest,
     reply: FastifyReply
   ) {
-    throw new Error('Not Implemented');
-  }
+    const paramSchema = z.object({
+      id: z.coerce.number().int().positive()
+    });
 
+    const { id } = paramSchema.parse(request.params);
+
+    const usecase = UseCaseFactory.setTodoHasDoneUseCase;
+    await usecase.execute(id);
+
+    reply.code(204).send();
+  }
 }
